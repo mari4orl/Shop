884b9338887b5505186ded615ab495073f993f23
mari4orl <mari4orl@mail.ru>
2022-06-24 15:32:27 +0300

    Initial commit

---
+++ ODataBackend/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IIS.Shop
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// Основной класс приложения.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Точка входа в приложение.
+        /// </summary>
+        /// <param name="args">Аргументы запуска.</param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Создать инициализатор приложения.
+        /// </summary>
+        /// <param name="args">Аргументы запуска.</param>
+        /// <returns>Инициализатор приложения.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ ODataBackend/Startup.cs
@@ -0,0 +1,124 @@
+﻿namespace IIS.Shop
+{
+    using System;
+    using ICSSoft.Services;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+    using ICSSoft.STORMNET.Security;
+    using IIS.Caseberry.Logging.Objects;
+    using Microsoft.AspNet.OData.Extensions;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using NewPlatform.Flexberry.ORM.ODataService.Extensions;
+    using NewPlatform.Flexberry.ORM.ODataService.Files;
+    using NewPlatform.Flexberry.ORM.ODataService.Model;
+    using NewPlatform.Flexberry.ORM.ODataService.WebApi.Extensions;
+    using NewPlatform.Flexberry.ORM.ODataServiceCore.Common.Exceptions;
+    using NewPlatform.Flexberry.Services;
+
+    /// <summary>
+    /// Класс настройки запуска приложения.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="configuration">An application configuration properties.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// An application configuration properties.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Configurate application services.
+        /// </summary>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </remarks>
+        /// <param name="services">An collection of application services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connStr = Configuration["DefConnStr"];
+            services.AddSingleton<ISecurityManager, EmptySecurityManager>();
+            services.AddSingleton<IDataService, PostgresDataService>(f => new PostgresDataService(f.GetService<ISecurityManager>()) { CustomizationString = connStr });
+            services.AddTransient<ILockService, NewPlatform.Flexberry.Services.LockService>();
+
+            services.AddMvcCore(
+                    options =>
+                    {
+                        options.Filters.Add<CustomExceptionFilter>();
+                        options.EnableEndpointRouting = false;
+                    })
+                .AddFormatterMappings();
+
+            services.AddOData();
+
+            services.AddSingleton<IDataObjectFileAccessor>(
+                provider =>
+                {
+                    const string fileControllerPath = "odata/file";
+                    string baseUriRaw = Configuration["BackendRoot"];
+                    var baseUri = new Uri(baseUriRaw);
+                    string uploadPath = Configuration["UploadUrl"];
+                    return new DefaultDataObjectFileAccessor(
+                        baseUri,
+                        fileControllerPath,
+                        uploadPath);
+                });
+
+            services.AddControllers().AddControllersAsServices();
+
+            services.AddCors();
+            services
+                .AddHealthChecks()
+                .AddNpgSql(connStr);
+        }
+
+        /// <summary>
+        /// Configurate the HTTP request pipeline.
+        /// </summary>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </remarks>
+        /// <param name="app">An application configurator.</param>
+        /// <param name="env">Information about web hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            LogService.LogInfo("Инициирован запуск приложения.");
+
+            app.UseRouting();
+
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+
+            app.UseODataService(builder =>
+            {
+                builder.MapFileRoute();
+
+                var assemblies = new[]
+                {
+                    typeof(ObjectsMarker).Assembly,
+                    typeof(ApplicationLog).Assembly,
+                    typeof(UserSetting).Assembly,
+                    typeof(Lock).Assembly,
+                };
+                var modelBuilder = new DefaultDataObjectEdmModelBuilder(assemblies, false);
+
+                var token = builder.MapDataObjectRoute(modelBuilder);
+            });
+        }
+    }
+}
